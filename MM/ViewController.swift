a435e3ece491d56faa975c47833083c2aafbdaa9
Aidan Maldonado <aidanm1198@gmail.com>
2019-03-16 11:23:31 -0400

    Basic Set up

--- MM/ViewController.swift
+++ MM/ViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mats = 0
+    var marauders = 0
+    
+    @IBOutlet weak var matsLabel: UILabel!
+    @IBOutlet weak var maraudersLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
 
 }
